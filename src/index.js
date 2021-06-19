ab96c11eb9f683868a4519dfc80f64921c3cda47
Ramesh Meduri <ramesh.meduri@gmail.com>
2021-06-19 17:33:46 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import CollapsiblePanel from './Components/CollapsiblePanel';
+import Grid from './Components/Grid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <div className="container">
-    <CollapsiblePanel title="Client" isOpen={true}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </CollapsiblePanel>
+    <Grid />
   </div>
 );
 
