ecb709deceb048f18064100e9b5ad225382cb2ca
Ramesh Meduri <ramesh.meduri@gmail.com>
2021-06-30 00:28:19 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Grid from './Components/Grid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <div className="container">
-    <Grid />
+    <h1>test</h1>
   </div>
 );
 
